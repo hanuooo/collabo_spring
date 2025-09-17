8c4588f399fafd7437908dd56764b4d29c61f78d
allright <allright@example.com>
2025-09-17 15:52:52 +0900

    spring 2025/09/17

--- src/main/java/com/coffee/controller/ElementController.java
+++ src/main/java/com/coffee/controller/ElementController.java
@@ -37,8 +37,8 @@ import java.util.List;
         elementList.add(new Element(4, "카푸치노", 4000, "beverage",444,"product_1740054104896.jpg","스팀밀크와 거품을 올린 것을 섞어 만든 이탈리아의 전통적인 커피 음료입니다."));
         elementList.add(new Element(4, "스폰지 케이크", 5000, "cake",555,"sponge_cake_01.png","가장 일반적인 케이크로, 부드럽고 공기가 많은 스폰지 텍스처를 가지고 있습니다. 일반적으로 크림, 과일, 초콜릿 등 다양한 토핑과 함께 제공됩니다."));
         elementList.add(new Element(5, "초콜릿 케이크", 6000, "cake",666,"chocolate_cake_01.png","초콜릿으로 만든 케이크로, 풍부하고 진한 초콜릿 맛을 가지고 있습니다. 초콜릿으로 만든 케이크 스폰지와 초콜릿으로 만든 크림 또는 가나슈를 사용하여 제작됩니다."));
-        elementList.add(new Element(6, "바닐라 마카롱", 7000, "macaron",777,"vanilla_latte_02.png","부드럽고 달콤한 바닐라 크림이 들어 있는 프랑스식 디저트입니다. 겉은 바삭하고 속은 촉촉한 식감이 특징입니다."));
-        elementList.add(new Element(6, "딸기 마카롱", 8000, "macaron",888,"","상큼한 딸기 크림이 가득 들어 있는 마카롱으로, 달콤하면서도 상큼한 맛을 즐길 수 있습니다."));
+        elementList.add(new Element(6, "바닐라 마카롱", 7000, "macaron",777,"macaron01.png","부드럽고 달콤한 바닐라 크림이 들어 있는 프랑스식 디저트입니다. 겉은 바삭하고 속은 촉촉한 식감이 특징입니다."));
+        elementList.add(new Element(6, "딸기 마카롱", 8000, "macaron",888,"macaron01.png","상큼한 딸기 크림이 가득 들어 있는 마카롱으로, 달콤하면서도 상큼한 맛을 즐길 수 있습니다."));
 
         return elementList;
     }
