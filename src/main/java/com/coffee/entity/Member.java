50384a0e740c09eacb42becd7261abdf3e452229
allright <allright@example.com>
2025-09-18 17:08:11 +0900

    spring 2025/09/18

---
+++ src/main/java/com/coffee/entity/Member.java
@@ -0,0 +1,37 @@
+package com.coffee.entity;
+
+import com.coffee.constant.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+
+
+// 회원 1명에 대한 정보를 저장하고 있는 자바 클래스
+@Getter @Setter @ToString // lombok으로.
+@Entity // 해당 클래스를 @Entity 로 관리 해주세요.
+@Table(name = "members") // 테이블 이름은 "members"으로 생성해 주세요.
+public class Member {
+    @Id // 이 컬럼은 primary key입니다.
+    @GeneratedValue(strategy = GenerationType.AUTO)// 기본키 생성 전략
+    @Column(name="member_id")// 컬럼 이름을 변경합니다.
+    private Long id;
+
+    private String name;
+
+    // 필수 사항이고, 절대로 동일한 값이 들어오면 안됩니다.
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // 일반인 또는 관리자
+
+    private LocalDate regdate ; // 등록일자
+}
---
+++ src/main/java/com/coffee/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.coffee.repository;
+
+import com.coffee.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 회원 정보들을 이용하여 데이터베이스와 교신하는 인터페이스입니다.
+// 이전의 Dao 역할
+// JpaRepository<엔터티이름, 해당엔터티의 기본 키 변수의 타입>
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
