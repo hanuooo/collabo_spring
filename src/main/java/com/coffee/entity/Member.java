db5b084efacaaa495cee186e5672c0a0f15c4828
allright <allright@example.com>
2025-09-22 17:06:10 +0900

    spring 2025/09/22

--- src/main/java/com/coffee/entity/Member.java
+++ src/main/java/com/coffee/entity/Member.java
@@ -31,7 +31,7 @@ public class Member {
     @Email(message = "올바른 이메일 형식으로 입력해 주셔야 합니다.")
     private String email;
 
-    //@Pattern은 정규 표현식을 의미합니다.
+    // @Pattern은 정규 표현식을 의미합니다.
     // [asdf] : 대괄호 사이에 들어 있는데 글자 1개와 매칭
     @NotBlank(message = "비밀번호는 필수 입력 사항입니다.")
     @Size(min = 8, max = 255, message = "비밀번호는 8자리 이상, 255자리 이하로 입력해 주세요.")
